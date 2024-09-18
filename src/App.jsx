ac0c13067bdef7820c12eab5fbcd7221361ee7f5
jacksonnsaini <jsaini34@my.bcit.ca>
2024-09-17 23:34:20 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,214 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="titleContainer">
+          <h1 className="title">Jacksons Album Lab</h1>
+        </div>
+        <div className="innerContainer">
+          <div className="colLeft">
+            <div className="image-container">
+              <img className='artistPortrait' src='https://media.gq.com/photos/64c81b2827469240470ea871/16:9/w_2560%2Cc_limit/story%2520dnc%2520230718_GQ_LUV_02_0205_V2_HR.jpg'></img>
+            </div>
+          </div>
+          <div className="colRight">
+          <img className='lilUziWordmark' src="../public/Images/Asset11.png"></img>
+          <h2>Discography</h2>
+          <details>
+  <summary><em>Luv Is Rage</em> (2015)</summary>
+  <ol>
+    <li>Safe House</li>
+    <li>Banned From TV</li>
+    <li>Super Saiyan</li>
+    <li>7AM</li>
+    <li>Right Now</li>
+    <li>Moist</li>
+    <li>Top</li>
+    <li>Enemies</li>
+    <li>Belly</li>
+    <li>Wit My Crew x 1987</li>
+    <li>Queso (feat. Wiz Khalifa)</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Lil Uzi Vert vs. the World</em> (2016)</summary>
+  <ol>
+    <li>Canadian Goose</li>
+    <li>Hi Roller</li>
+    <li>Money Longer</li>
+    <li>Grab the Wheel</li>
+    <li>You Was Right</li>
+    <li>Babe Don't Lie</li>
+    <li>Ps & Qs</li>
+    <li>Team Rocket</li>
+    <li>Scott and Ramona</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>The Perfect LUV Tape</em> (2016)</summary>
+  <ol>
+    <li>Do What I Want</li>
+    <li>Of Course We Ghetto Flowers (feat. Playboi Carti)</li>
+    <li>Original Uzi (4 of Us)</li>
+    <li>Money Mitch</li>
+    <li>SideLine Watching (Hold Up)</li>
+    <li>Alpha Omega</li>
+    <li>Erase Your Social</li>
+    <li>Ronda (Winners)</li>
+    <li>Seven Million (feat. Future)</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Luv Is Rage 2</em> (2017)</summary>
+  <ol>
+    <li>Two®</li>
+    <li>444+222</li>
+    <li>Sauce It Up</li>
+    <li>No Sleep Leak</li>
+    <li>The Way Life Goes</li>
+    <li>For Real</li>
+    <li>Feelings Mutual</li>
+    <li>Neon Guts (feat. Pharrell Williams)</li>
+    <li>Early 20 Rager</li>
+    <li>UnFazed (feat. The Weeknd)</li>
+    <li>Pretty Mami</li>
+    <li>How to Talk</li>
+    <li>X</li>
+    <li>Malfunction</li>
+    <li>Dark Queen</li>
+    <li>Xo Tour Llif3</li>
+    <li>Skir Skirr (Bonus Track)</li>
+    <li>Loaded (Bonus Track)</li>
+    <li>Diamonds All on My Wrist (Bonus Track)</li>
+    <li>20 Min (Bonus Track)</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Eternal Atake</em> (2020)</summary>
+  <ol>
+    <li>Baby Pluto</li>
+    <li>Lo Mein</li>
+    <li>Silly Watch</li>
+    <li>POP</li>
+    <li>You Better Move</li>
+    <li>Homecoming</li>
+    <li>I'm Sorry</li>
+    <li>Celebration Station</li>
+    <li>Bigger Than Life</li>
+    <li>Chrome Heart Tags</li>
+    <li>Bust Me</li>
+    <li>Prices</li>
+    <li>Urgency (feat. Syd)</li>
+    <li>Venetia</li>
+    <li>Secure the Bag</li>
+    <li>P2</li>
+    <li>Futsal Shuffle 2020</li>
+    <li>That Way</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Eternal Atake (Deluxe) – Lil Uzi Vert vs. The World 2</em> (2020)</summary>
+  <ol>
+    <li>Myron</li>
+    <li>Lotus</li>
+    <li>Bean (Kobe) [feat. Chief Keef]</li>
+    <li>Yessirskiii (feat. 21 Savage)</li>
+    <li>Wassup (feat. Future)</li>
+    <li>Strawberry Peels (feat. Young Thug & Gunna)</li>
+    <li>I Can Show You</li>
+    <li>Moon Relate</li>
+    <li>Come This Way</li>
+    <li>Trap This Way (This Way)</li>
+    <li>No Auto (feat. Lil Durk)</li>
+    <li>Money Spread (feat. Young Nudy)</li>
+    <li>Got the Guap (feat. Young Thug)</li>
+    <li>Leaders (feat. NAV)</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Pluto x Baby Pluto</em> (with Future) (2020)</summary>
+  <ol>
+    <li>Stripes Like Burberry</li>
+    <li>Marni on Me</li>
+    <li>Sleeping on the Floor</li>
+    <li>Real Baby Pluto</li>
+    <li>Drankin N Smokin</li>
+    <li>Million Dollar Play</li>
+    <li>Plastic</li>
+    <li>That's It</li>
+    <li>Bought a Bad Bitch</li>
+    <li>Rockstar Chainz</li>
+    <li>Lullaby</li>
+    <li>She Never Been to Pluto</li>
+    <li>Off Dat</li>
+    <li>I Don't Wanna Break Up</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Pluto x Baby Pluto (Deluxe)</em> (with Future) (2020)</summary>
+  <ol>
+    <li>Tic Tac</li>
+    <li>My Legacy</li>
+    <li>Heart in Pieces</li>
+    <li>Because of You</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Red & White</em> (2022)</summary>
+  <ol>
+    <li>Space Cadet</li>
+    <li>I Know</li>
+    <li>Flex Up</li>
+    <li>Hittin My Shoulder</li>
+    <li>For Fun</li>
+    <li>Cigarette</li>
+    <li>Believe Me</li>
+    <li>Issa Hit</li>
+    <li>Final Fantasy</li>
+  </ol>
+</details>
+
+<details>
+  <summary><em>Pink Tape</em> (2023)</summary>
+  <ol>
+    <li>Flooded the Face</li>
+    <li>Aye (feat. Travis Scott)</li>
+    <li>Crush Em</li>
+    <li>Suicide Doors</li>
+    <li>Endless Fashion (feat. Nicki Minaj)</li>
+    <li>Spin Again</li>
+    <li>That Fiya</li>
+    <li>Zoom</li>
+    <li>Of Course</li>
+    <li>Patience (feat. Don Toliver)</li>
+    <li>Days Come and Go</li>
+    <li>Rehab</li>
+    <li>Shrooms</li>
+    <li>Werewolf (feat. Bring Me The Horizon)</li>
+    <li>Pluto to Mars</li>
+    <li>NBA</li>
+    <li>Margin for Error</li>
+    <li>Flooded the Face (Bonus Track)</li>
+    <li>Simp</li>
+  </ol>
+</details>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
